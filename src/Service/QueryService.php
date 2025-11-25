81259bb597e414c55c66bc9fcf1d203f7a49bcd3
Brenden Tamilio <btamilio@gmail.com>
2025-11-24 22:15:35 -0500

    incr

--- src/Service/QueryService.php
+++ src/Service/QueryService.php
@@ -2,43 +2,38 @@
 
 namespace Btamilio\HsDummyJson\Service;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Client\Factory as HttpFactory;
+use Illuminate\Http\Request;
 use Illuminate\Translation\ArrayLoader;
 use Illuminate\Translation\Translator;
 use Illuminate\Validation\Factory;
-
 use Illuminate\Validation\ValidationException;
 use Exception;
 
-
 class QueryService
 {
-
-    public function validate(Request $request) 
+    public function validate(Request $request): array
     {
+        // Build a minimal validator instance outside the Laravel container
+        $validator = (new Factory(new Translator(new ArrayLoader(), 'en')))->make(
+            $request->all(),
+            [
+                'q' => 'required|string|max:100',
+            ]
+        );
 
- 
-        try {
-            $request->validate( [
-                 'q' => 'required|string|max:100',
-        ]);
-        } catch (ValidationException $e) {
-            return  ["errors" => $e->getMessage() ];
-        } catch (Exception $e) {
-            return  ["errors" => "Could not validate request" ];
-        }   
-
+        return $validator->validate();
     }
 
     public function search(array $input): array
     {
- 
         $http = new HttpFactory();
-        return $http->get('https://dummyjson.com/users/search', ['q' => $input['q'] ?? ''])->json();
+
+        return $http
+            ->get('https://dummyjson.com/users/search', ['q' => $input['q'] ?? ''])
+            ->json();
     }
 
  
 }
 
- 
